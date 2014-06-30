ff5998377d5bbc1d99e4148e354de7675c50d669
sskates1 <simonirv2@gmail.com>
2014-06-30 01:23:02 -0400

    added erb files and fleshed out controller

--- app/controllers/manufacturer_controller.rb
+++ app/controllers/manufacturer_controller.rb
@@ -1,2 +1,30 @@
-class ManufacturerController < ApplicationController
+class ManufacturersController < ApplicationController
+  def index
+    @manufacturers = Manufacturer.all
+  end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def create
+    @manufacturer = manufacturers.new(manufacturer_params)
+
+    if @manufacturer.save
+      redirect_to manufacturers_path
+    else
+      flash.now[:notice] = 'Uh oh! Your manufacturer could not be saved.'
+      render :new
+    end
+  end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :country)
+  end
 end
--- db/migrate/20140630045007_create_manufacturers.rb
+++ db/migrate/20140630045007_create_manufacturers.rb
@@ -1,6 +1,8 @@
 class CreateManufacturers < ActiveRecord::Migration
   def change
     create_table :manufacturers do |t|
+      t.string :country, null: false
+      t.string :name, null: false
 
       t.timestamps
     end
