a14c333b6ba5b91223930f4a1dd9a08f898b0cf6
Helen Hood <helen.c.hood@gmail.com>
2014-07-02 11:07:05 -0400

    Add test for uniqueness constaint on manufacturer

--- spec/features/user_creates_manufacturer_spec.rb
+++ spec/features/user_creates_manufacturer_spec.rb
@@ -24,4 +24,17 @@ feature 'user adds a new manufacturer', %Q(
     expect(page).to have_content "Name can't be blank"
     expect(page).to have_content "Country can't be blank"
   end
+
+  scenario 'user tries to add a manufacturer already in database' do
+    manufacturer = FactoryGirl.create(:manufacturer)
+
+    visit new_manufacturer_path
+
+    fill_in 'Name', with: manufacturer.name
+    fill_in 'Country', with: manufacturer.country
+    click_on 'Submit'
+
+    expect(page).to have_content "has already been taken"
+    expect(page).to_not have_content "Your manufacturer  was successfully entered"
+  end
 end
